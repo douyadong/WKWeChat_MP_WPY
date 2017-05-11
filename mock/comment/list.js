5520989c31b390ceb3e5fb4abf99278edc452760
oscar <zengxiangzhi@lifang.com>
2017-05-11 16:07:59 +0800

    commit

---
+++ mock/comment/list.js
@@ -0,0 +1,311 @@
+module.exports = {
+	"status": 1,
+	"count": 0,
+	"message": "成功",
+	"data": {
+		"subEstateId": 12465,
+		"amount": 10,
+		"commentList": [{
+			"pkid": 322,
+			"subEstateId": 0,
+			"upAmount": 4,
+			"downAmount": 0,
+			"comment": "大哥大姐东看西看出成绩从哪放假姐姐",
+			"createTimeStr": "2016-04-19 10:33:47",
+			"orderLevel": null,
+			"isUp": 1,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": [
+				'https://img.wkzf.com/f776bae5187a46898f5750bc68ac4339.DL',
+				'https://img.wkzf.com/0207844ddfa34cdf965d963a816907f8.DL'
+			],
+			"landlord": 0
+		}, {
+			"pkid": 321,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "愁更愁交叉口错了错了差距就粗卡卡擦",
+			"createTimeStr": "2016-04-19 10:33:06",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 320,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "徐薪火相传坎坎坷坷",
+			"createTimeStr": "2016-04-19 10:30:04",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 297,
+			"subEstateId": 0,
+			"upAmount": 2,
+			"downAmount": 0,
+			"comment": "v电话打击打击飞机飞机飞机",
+			"createTimeStr": "2016-04-18 14:07:25",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18747,
+				"guestId": 18747,
+				"guestPhoneNum": "136****6163",
+				"name": "陈晓",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": null
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 288,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "德国加快看看德国进口法国进口",
+			"createTimeStr": "2016-04-15 11:33:43",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 286,
+			"subEstateId": 0,
+			"upAmount": 1,
+			"downAmount": 1,
+			"comment": "对国家和地方记录的虚假看看",
+			"createTimeStr": "2016-04-15 11:33:30",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 285,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "得干净利落历历可见吧",
+			"createTimeStr": "2016-04-15 11:33:10",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 284,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "德国加快了科技经济",
+			"createTimeStr": "2016-04-15 11:33:01",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": [{
+				"pkid": 123,
+				"url": "http://imgwater-test.oss.aliyuncs.com/6b706135c8b24a769e08662bb33ee5a5",
+				"imgKey": "6b706135c8b24a769e08662bb33ee5a5",
+				"postFix": null,
+				"standard": null
+			}, {
+				"pkid": 124,
+				"url": "http://imgwater-test.oss.aliyuncs.com/e28438fa249c483b83a8e22431de16cd",
+				"imgKey": "e28438fa249c483b83a8e22431de16cd",
+				"postFix": null,
+				"standard": null
+			}],
+			"landlord": 0
+		}, {
+			"pkid": 283,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 0,
+			"comment": "复活节快乐了",
+			"createTimeStr": "2016-04-15 11:32:37",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18487,
+				"guestId": 18487,
+				"guestPhoneNum": "137****6162",
+				"name": "陈词",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": {
+					"pkid": 283,
+					"url": "http://imgwater-test.oss.aliyuncs.com/ff70910a8e2349e2a2e93cea864c05a7",
+					"imgKey": "ff70910a8e2349e2a2e93cea864c05a7",
+					"standard": "",
+					"postFix": "",
+					"imgType": 1
+				}
+			},
+			"imgList": null,
+			"landlord": 0
+		}, {
+			"pkid": 254,
+			"subEstateId": 0,
+			"upAmount": 0,
+			"downAmount": 1,
+			"comment": "大家看空间",
+			"createTimeStr": "2016-04-06 14:43:04",
+			"orderLevel": null,
+			"isUp": 0,
+			"isDown": 0,
+			"guest": {
+				"pkid": 18453,
+				"guestId": 18453,
+				"guestPhoneNum": "136****2626",
+				"name": "张罗",
+				"sex": 1,
+				"openId": null,
+				"unionId": null,
+				"systemAvatarId": null,
+				"avatar": null
+			},
+			"imgList": null,
+			"landlord": 0
+		}]
+	}
+}
--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -5,6 +5,11 @@
 const domain = 'https://m.wkzf.com'
 
 const url = {
+  'agent':{
+    'rateList':'/wxmpAgent/getAgentCustomerCommentList.rest',
+    'getAgentInfo':'/wxmpAgent/getSimpleAgent.rest',
+    'writeRate':'/wxmpAgent/commentAgent.rest',
+  },
   'buy': {
     'getDetails': '/esf/moreList.rest?offset=10&pinyin=shanghai&_=14943219',
     'add':"/addInterestRecord.rest",
@@ -20,6 +25,11 @@ const url = {
   	'getDetails':'/wxmpOldHouseDetail/getOldHouseDetail.rest',
     'similar':'/wxmpOldHouseDetail/getSimilarList.rest',
     'agentInfo':'/wxmpAgent/getSimpleAgent.rest'
+  },
+  'comment':{
+    'list':'/estate/queryEstateCommentList.rest',
+    'uploadImg':'/wxmpEstate/uploadPic.rest',
+    'uploadComment':'/wxmpEstate/commentEstate.rest'
   }
 }
 
