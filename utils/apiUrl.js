0c8d80de839f50980837d6a4391d4f430d4e6b6a
赵 华刚 <zhaohuagang@lifang.com>
2017-05-11 17:52:01 +0800

    mp

--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -2,34 +2,35 @@
  * @desc:统一定义请求接口地址
  */
 
-const domain = 'https://m.wkzf.com'
+const domain = "https://m.wkzf.com" ;
 
 const url = {
-  'agent':{
-    'rateList':'/wxmpAgent/getAgentCustomerCommentList.rest',
-    'getAgentInfo':'/wxmpAgent/getSimpleAgent.rest',
-    'writeRate':'/wxmpAgent/commentAgent.rest',
-  },
-  'buy': {
-    'getDetails': '/esf/moreList.rest?offset=10&pinyin=shanghai&_=14943219',
-    'add':"/addInterestRecord.rest",
-    'edit':'/updateInterestRecord.rest',
-    'getAgentList':''
-  },
-  'estate':{
-    'zan':'/wxmpEstate/upOrDownEstateComment.rest',
-    'detail':'/wxmpEstate/estateDetail.rest',
-    'sellingList':'/wxmpEstate/estateSellingHouseList.rest'
-  },
-  'esf':{
-  	'getDetails':'/wxmpOldHouseDetail/getOldHouseDetail.rest',
-    'similar':'/wxmpOldHouseDetail/getSimilarList.rest',
-    'agentInfo':'/wxmpAgent/getSimpleAgent.rest'
-  },
-  'comment':{
-    'list':'/estate/queryEstateCommentList.rest',
-    'uploadImg':'/wxmpEstate/uploadPic.rest',
-    'uploadComment':'/wxmpEstate/commentEstate.rest'
+  "agent" : {
+    "rateList" : "/wxmpAgent/getAgentCustomerCommentList.rest",
+    "getAgentInfo" : "/wxmpAgent/getSimpleAgent.rest",
+    "writeRate" : "/wxmpAgent/commentAgent.rest",
+    "detail" : "/wxmpAgent/getAgentDetail.rest"
+  } ,
+  "buy" : {
+    "getDetails" : "/esf/moreList.rest?offset=10&pinyin=shanghai&_=14943219" ,
+    "add" : "/addInterestRecord.rest" ,
+    "edit" : "/updateInterestRecord.rest" ,
+    "getAgentList" : ""
+  } ,
+  "estate" : {
+    "zan" : "/wxmpEstate/upOrDownEstateComment.rest" ,
+    "detail" : "/wxmpEstate/estateDetail.rest" ,
+    "sellingList" : "/wxmpEstate/estateSellingHouseList.rest"
+  } ,
+  "esf" : {
+  	"getDetails" : "/wxmpOldHouseDetail/getOldHouseDetail.rest" ,
+    "similar" : "/wxmpOldHouseDetail/getSimilarList.rest" ,
+    "agentInfo" : "/wxmpAgent/getSimpleAgent.rest"
+  } ,
+  "comment" : {
+    "list" : "/estate/queryEstateCommentList.rest" ,
+    "uploadImg" : "/wxmpEstate/uploadPic.rest" ,
+    "uploadComment" : "/wxmpEstate/commentEstate.rest"
   }
 }
 
