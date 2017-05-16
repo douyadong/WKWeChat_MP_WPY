1a65245d41a71702a191d05ad3dcc9fd451a8d1c
oscar <zengxiangzhi@lifang.com>
2017-05-16 18:49:21 +0800

    commit

--- pages/components/detailfoot.js
+++ pages/components/detailfoot.js
@@ -1,7 +1,7 @@
 module.exports ={
 	data:{
 		agentInfo: {
-            "headRoundImgUrl": "https://img.wkzf.com/a365d1ae298c453f914ca45f910aa175",
+            "headRoundImgUrl":"https://img.wkzf.com/a365d1ae298c453f914ca45f910aa175",
             "agentName": '陈元杰',
             "abbreviation": '志远地产',
             "mobild":'13989890909',
@@ -26,7 +26,7 @@ module.exports ={
 	},
 	c_df_wechatClick:function(e){
 		wx.navigateTo({
-	        url: '../agent/qrcode'
+	        url: '../agent/qrcode?agentId'+this.data.agentInfo.agentId
       	})
 	},
 	c_df_phoneClick:function(){
