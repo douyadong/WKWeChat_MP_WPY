c20e14dedc2c885e0046d4a1d6d3d793e8047825
赵 华刚 <zhaohuagang@lifang.com>
2017-05-16 20:24:33 +0800

    mp

--- pages/components/detailfoot.js
+++ pages/components/detailfoot.js
@@ -1,16 +1,6 @@
 module.exports ={
 	data:{
-		agentInfo: {
-            "headRoundImgUrl":"https://img.wkzf.com/a365d1ae298c453f914ca45f910aa175",
-            "agentName": '陈元杰',
-            "abbreviation": '志远地产',
-            "mobild":'13989890909',
-            "weChatQRImgKey":'13989890909',
-            "agentWChartQRImgUrl":"https://imgwater.oss.aliyuncs.com/67d28c90288a43238d311f2942d4f029",
-            'isShowWXCode':true,
-            "agentWChatId":''
-        }
-	},
+	} ,
 	c_df_agentClick:function(e){
 		var _this = this,
 			scene = wx.getStorageSync('scene');
@@ -26,7 +16,7 @@ module.exports ={
 	},
 	c_df_wechatClick:function(e){
 		wx.navigateTo({
-	        url: '../agent/qrcode?agentId'+this.data.agentInfo.agentId
+	        url: '../agent/qrcode?agentId='+this.data.agentInfo.agentId
       	})
 	},
 	c_df_phoneClick:function(){
