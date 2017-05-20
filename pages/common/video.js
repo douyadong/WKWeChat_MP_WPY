f39d714b692c180ccee0e9e633355ccf99605776
yinqin <yinqin@lifang.com>
2017-05-20 15:53:39 +0800

    视频播放页面底部经纪人

--- pages/common/video.js
+++ pages/common/video.js
@@ -2,11 +2,10 @@
 var util = require('../../utils/util.js');
 var dt = require('../components/detailfoot.js');
 var request = require('../../utils/request.js');
-Page({
-    data: {
-        // videoUrl:'http://v.wkzf.com/fe5b05415e74492f93752219333d443bWV.mp4'
-    },
-    onLoad: function (options) {
+var $ = require('../../utils/extend.js');
+var params = $.extend(true, {}, {
+    data: {},
+    onLoad: function(options) {
         this.setData({
             videoUrl: options.videoUrl,
             agentId: options.agentId
@@ -19,7 +18,7 @@ Page({
             title: '买房卖房，找好经纪人就对了！'
         }
     },
-    getAgentInfo: function () {
+    getAgentInfo: function() {
         var that = this;
         if (this.data.agentId) {
             request.fetch({
@@ -28,23 +27,24 @@ Page({
                 data: {
                     agentId: this.data.agentId
                 },
-                success: function (data) {
+                success: function(data) {
                     data.data.isShowWXCode = false;
                     that.setData({
-                        agentInfo:data.data
-                        /*agentInfo: {
-                            "agentImage": data.data.headRoundImgUrl,
-                            "agentName": data.data.agentName,
-                            "agentCompany": data.data.companyName,
-                            "agentPhoneNum": data.data.agentMobile,
-                            "agentWechatNum": data.data.agentWChatId,
-                            "agentCodeImg": data.data.agentWChartQRImgUrl,
-                            'isShowWXCode': false
-                        }*/
+                        agentInfo: data.data
+                            /*agentInfo: {
+                                "agentImage": data.data.headRoundImgUrl,
+                                "agentName": data.data.agentName,
+                                "agentCompany": data.data.companyName,
+                                "agentPhoneNum": data.data.agentMobile,
+                                "agentWechatNum": data.data.agentWChatId,
+                                "agentCodeImg": data.data.agentWChartQRImgUrl,
+                                'isShowWXCode': false
+                            }*/
                     });
 
                 }
             });
         }
     }
-})
+}, dt);
+Page(params)
